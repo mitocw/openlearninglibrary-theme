45bcb08005b8b32e4773bf77afc842dea900792b
Muhammad Amjad <muhammad.amjad@arbisoft.com>
2019-06-14 02:43:31 +0500

    Add styling for footer along logged in user header. (#2)

--- mitx/lms/static/js/accordion.js
+++ mitx/lms/static/js/accordion.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 		$('.accordion-content').not($(this).parent().find('.accordion-content')).slideUp(400, "swing");
 		$(this).parent().find('.accordion-content').slideToggle(400, "swing");
 	});
+
+	$('#btn_expand_faqs').click(function() {
+		$('.accordion-content').slideDown(400, "swing");
+	});
+
+	$('#btn_minimise_faqs').click(function() {
+		$('.accordion-content').slideUp(400, "swing");
+	});
 });
