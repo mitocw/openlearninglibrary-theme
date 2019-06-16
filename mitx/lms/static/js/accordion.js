0678843e46650d956bf6dbb79c8504046de33689
mamjad00 <muhammad.amjad@arbisoft.com>
2019-06-17 01:36:28 +0500

    Add styling for login regiter apge.

--- mitx/lms/static/js/accordion.js
+++ mitx/lms/static/js/accordion.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-	$('.accordion-title').click(function() {
-		$('.accordion-content').not($(this).parent().find('.accordion-content')).slideUp(400, "swing");
-		$(this).parent().find('.accordion-content').slideToggle(400, "swing");
-	});
+    $('.accordion-title').click(function() {
+        $(this).parent().find('.accordion-content').slideToggle(400, "swing");
+    });
 
-	$('#btn_expand_faqs').click(function() {
-		$('.accordion-content').slideDown(400, "swing");
-	});
+    $('#btn_expand_faqs').click(function() {
+        $('.accordion-content').slideDown(400, "swing");
+    });
 
-	$('#btn_minimise_faqs').click(function() {
-		$('.accordion-content').slideUp(400, "swing");
-	});
+    $('#btn_minimise_faqs').click(function() {
+        $('.accordion-content').slideUp(400, "swing");
+    });
 });
