16d5c64eac5c7f82b9cea77e5b17cae0e5e87981
mamjad00 <muhammad.amjad@arbisoft.com>
2019-06-25 02:05:56 +0500

    Course about page fixes

--- mitx/lms/static/js/login-register.js
+++ mitx/lms/static/js/login-register.js
@@ -1,104 +1,93 @@
-window.onload = function () {
-    var postRender = function () {
+var postRender = function () {
+    $("label").addClass("focus-in").removeClass("focus-out");
+    var inputs = this.$('.form-field'),
+        inputSelectors = 'input, select, textarea',
+        inputTipSelectors = ['tip error', 'tip tip-input'],
+        inputTipSelectorsHidden = ['tip error hidden', 'tip tip-input hidden'],
+        onInputFocus = function () {
+            // Apply on focus styles to input
+            $(this).find('label').addClass('focus-in').removeClass('focus-out');
 
-        $("label").addClass("focus-in").removeClass("focus-out");
-        var inputs = this.$('.form-field'),
-            inputSelectors = 'input, select, textarea',
-            inputTipSelectors = ['tip error', 'tip tip-input'],
-            inputTipSelectorsHidden = ['tip error hidden', 'tip tip-input hidden'],
-            onInputFocus = function () {
-                // Apply on focus styles to input
-                $(this).find('label').addClass('focus-in')
-                    .removeClass('focus-out');
-
-                // Show each input tip
-                $(this).children().each(function () {
-                    if (inputTipSelectorsHidden.indexOf($(this).attr('class')) >= 0) {
-                        $(this).removeClass('hidden');
-                    }
-                });
-            },
-            onInputFocusOut = function () {
-                // If input has no text apply focus out styles
-                if ($(this).find(inputSelectors).val().length === 0) {
-                    $(this).find('label').addClass('focus-out')
-                        .removeClass('focus-in');
+            // Show each input tip
+            $(this).children().each(function () {
+                if (inputTipSelectorsHidden.indexOf($(this).attr('class')) >= 0) {
+                    $(this).removeClass('hidden');
                 }
+            });
+        },
+        onInputFocusOut = function () {
+            // If input has no text apply focus out styles
+            if ($(this).find(inputSelectors).val().length === 0) {
+                $(this).find('label').addClass('focus-out').removeClass('focus-in');
+            }
 
-                // Hide each input tip
-                $(this).children().each(function () {
-                    // This is a 1 instead of 0 so the error message for a field is not
-                    // hidden on blur and only the help tip is hidden.
-                    if (inputTipSelectors.indexOf($(this).attr('class')) >= 1) {
-                        $(this).addClass('hidden');
-                    }
-                });
-            },
-            handleInputBehavior = function (input) {
-                // Initially put label in input
-                if (input.find(inputSelectors).val().length === 0) {
-                    input.find('label').addClass('focus-out')
-                        .removeClass('focus-in');
+            // Hide each input tip
+            $(this).children().each(function () {
+                // This is a 1 instead of 0 so the error message for a field is not
+                // hidden on blur and only the help tip is hidden.
+                if (inputTipSelectors.indexOf($(this).attr('class')) >= 1) {
+                    $(this).addClass('hidden');
                 }
+            });
+        },
+        handleInputBehavior = function (input) {
+            // Initially put label in input
+            if (input.find(inputSelectors).val().length === 0) {
+                input.find('label').addClass('focus-out').removeClass('focus-in');
+            }
 
-                // Initially hide each input tip
-                input.children().each(function () {
-                    if (inputTipSelectors.indexOf($(this).attr('class')) >= 0) {
-                        $(this).addClass('hidden');
-                    }
-                });
+            // Initially hide each input tip
+            input.children().each(function () {
+                if (inputTipSelectors.indexOf($(this).attr('class')) >= 0) {
+                    $(this).addClass('hidden');
+                }
+            });
 
-                input.focusin(onInputFocus);
-                input.focusout(onInputFocusOut);
-            },
-            handleAutocomplete = function () {
-                $(inputs).each(function () {
-                    var $input = $(this),
-                        isCheckbox = $input.attr('class').indexOf('checkbox') !== -1;
-
-                    if (!isCheckbox) {
-                        if ($input.find(inputSelectors).val().length === 0
-                            && !$input.is(':-webkit-autofill')) {
-                            $input.find('label').addClass('focus-out')
-                                .removeClass('focus-in');
-                        } else {
-                            $input.find('label').addClass('focus-in')
-                                .removeClass('focus-out');
-                        }
-                    }
-                });
-            };
+            input.focusin(onInputFocus);
+            input.focusout(onInputFocusOut);
+        },
+    handleAutocomplete = function () {
         $(inputs).each(function () {
             var $input = $(this),
                 isCheckbox = $input.attr('class').indexOf('checkbox') !== -1;
-            if ($input.length > 0 && !isCheckbox) {
-                handleInputBehavior($input);
+
+            if (!isCheckbox) {
+                if ($input.find(inputSelectors).val().length === 0
+                    && !$input.is(':-webkit-autofill')) {
+                    $input.find('label').addClass('focus-out').removeClass('focus-in');
+                } else {
+                    $input.find('label').addClass('focus-in').removeClass('focus-out');
+                }
             }
         });
-        setTimeout(handleAutocomplete, 1000);
-    };
+    }
+    $(inputs).each(function () {
+        var $input = $(this),
+            isCheckbox = $input.attr('class').indexOf('checkbox') !== -1;
+        if ($input.length > 0 && !isCheckbox) {
+            handleInputBehavior($input);
+        }
+    });
+    setTimeout(handleAutocomplete, 1000);
+};
+
+
+var periodicCaller = function () {
     var interval = setInterval(function () {
         if ($("label").length) {
             clearInterval(interval);
             postRender();
         }
     });
-
-    $("button.forgot-password").click(function () {
-        var interval = setInterval(function () {
-            if ($("label").length) {
-                clearInterval(interval);
-                postRender();
-            }
-        });
-    });
-
-    $("a.form-toggle").click(function () {
-        var interval = setInterval(function () {
-            if ($("label").length) {
-                clearInterval(interval);
-                postRender();
-            }
-        });
-    });
 }
+
+
+window.onload = function () {
+    periodicCaller();
+}
+
+$(document).on('click', function(event) {
+    if(event.target.nodeName == 'A' || event.target.nodeName == 'BUTTON'){
+        periodicCaller();
+    }
+});
