065310a8d88b2797acb18f79c37dde5c7e00bceb
mamjad00 <muhammad.amjad@arbisoft.com>
2019-06-26 19:44:37 +0500

    Add requested changes

---
+++ mitx/lms/static/js/smooth-scroll.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('.back-to-top').on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#terms-of-service').offset().top
+        }, 800);
+    });
+
+    var autoHeight = $('body').hasClass('auto-height');
+    if(autoHeight) {
+        $('html, body').css({
+            'height': 'auto'
+        });
+    }
+
+    $('.list-divided a').on('click', function(event) {
+        var scrollElement = $(this).attr('href');
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(scrollElement).offset().top
+        }, 800);
+    });
+});
