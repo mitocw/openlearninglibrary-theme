57cf5543fdcd8337500ccdae9d7505209046666e
danialmalik <danialmalik321@gmail.com>
2020-06-11 11:49:40 +0500

    FIX: exception in console logs

--- mitx/lms/static/js/smooth-scroll.js
+++ mitx/lms/static/js/smooth-scroll.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('.back-to-top').on('click', function(event) {
+$(document).ready(function () {
+    $('.back-to-top').on('click', function (event) {
         event.preventDefault();
         $('html, body').animate({
             scrollTop: $('#terms-of-service').offset().top
@@ -7,13 +7,13 @@ $(document).ready(function() {
     });
 
     var autoHeight = $('.auto-height').length > 0;
-    if(autoHeight) {
+    if (autoHeight) {
         $('html, body').css({
             'height': 'auto'
         });
     }
 
-    $('.list-divided a').on('click', function(event) {
+    $('.list-divided a').on('click', function (event) {
         var scrollElement = $(this).attr('href');
         event.preventDefault();
         $('html, body').animate({
@@ -25,10 +25,12 @@ $(document).ready(function() {
 /*
 ** Navigate to anchor on page (#hash link)
 */
-$(document).ready(function() {
+$(document).ready(function () {
     var target = $(location).attr("hash");
     var offset = ($(this).attr('data-offset') ? $(this).attr('data-offset') : 0);
-    $('body,html').animate({
-        scrollTop: $(target).offset().top - offset
-    }, 1000);
+    if (target) {
+        $('body,html').animate({
+            scrollTop: $(target).offset().top - offset
+        }, 1000);
+    }
 });
